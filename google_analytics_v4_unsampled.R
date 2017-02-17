4affd8042b8e1714806e3e17809bd1fe3f949b9f
khzt2004 <khzt@hotmail.com>
2017-02-17 11:07:50 +0800

    Add files via upload
    anti-sampling algorithm not perfected

--- google_analytics_v4_unsampled.R
+++ google_analytics_v4_unsampled.R
@@ -1,14 +1,15 @@
 # http://code.markedmondson.me/googleAnalyticsR/v4.html
 # http://kbroman.org/pkg_primer/pages/github.html
+# install_github("MarkEdmondson1234/googleAnalyticsR")
 
 library(googleAnalyticsR)
 ga_auth()
 ## get your accounts
 account_list <- google_analytics_account_list()
 id <- account_list[100,'viewId']
-unsampled_data_fetch <- google_analytics_4(id, date_range = c("2017-01-01", "2017-01-21"), 
-                                                             metrics = c("sessions","bounceRate"), 
-                                                             dimensions = c("date","source", "medium"),
-                                                             anti_sample = TRUE)
+unsampled_data_fetch <- google_analytics_4(id, date_range = c("2017-01-01", "2017-02-16"), 
+                                           metrics = c("sessions", "pageViews", "entrances", "bounceRate", "avgTimeOnPage", "exitRate"), 
+                                           dimensions = c("pagePath", "dimension2", "date"),
+                                           filtersExpression = "ga:dimension2==https://launch.solidworks.co.jp/",
+                                           anti_sample = TRUE)
 write.csv(unsampled_data_fetch, "My Data Export.csv")
-                                                             
