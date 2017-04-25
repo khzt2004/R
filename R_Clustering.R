f1f16a0f603fd3b33400f3f5b0353f383279a17d
khzt2004 <khzt@hotmail.com>
2017-04-25 16:53:36 +0800

    Add files via upload

--- R_Clustering.R
+++ R_Clustering.R
@@ -4,14 +4,25 @@
 
 library(data.table)
 library(readxl)
+library(XLConnect)
+library(cluster)
+library(factoextra)
 
 data <- read_excel("India_Keywords.xlsx")
 
-
-set.seed(76964057) #Set the seed for reproducibility
-k <-kmeans(data[,-c(1, 2, 4)], centers=5) #Create 5 clusters, remove column 2
+set.seed(123456789) #Set the seed for reproducibility
+k <-kmeans(data[,-c(1, 2, 4)], centers=4) #Create 5 clusters, remove column 2
 k$centers #Display&nbsp;cluster centers
 table(k$cluster) #Give a count of data points in each cluster
+
+data$cluster <- k$cluster
+
+# PCA
+res.pca <- prcomp(data[,-c(1, 2, 4)])
+get_eig(res.pca)
+# Default plot
+fviz_eig(res.pca)
+
 
 # scree plot
 
@@ -29,3 +40,29 @@ for(v in rng){ # For each value of the range variable
 plot(rng,avg.totw.ss,type="b", main="Total Within SS by Various K",
      ylab="Average Total Within Sum of Squares",
      xlab="Value of K")
+
+# plot clusters
+
+clusplot(data[,-c(1, 2, 4)], k$cluster, main='2D representation of the Cluster solution',
+         color=TRUE, shade=TRUE,
+         labels=2, lines=0)
+
+
+# export to excel
+fileDate <- format(Sys.time(), "%m%d%Y")
+fileName <- paste("India_Keywords_", fileDate, ".xlsx", sep="")
+
+#creating an Excel workbook. Both .xls and .xlsx file formats can be used.
+wb <- loadWorkbook(fileName, create = TRUE)
+
+#creating sheets within an Excel workbook
+createSheet(wb, name = "keywords")
+
+#writing into sheets within an Excel workbook : 
+#writing data frame into filteredVARs
+
+writeWorksheet(wb, data, sheet = "keywords", startRow = 1, startCol = 1)
+
+#saving a workbook to an Excel file :
+#saves a workbook to the corresponding Excel file and writes the file to disk.
+saveWorkbook(wb)
