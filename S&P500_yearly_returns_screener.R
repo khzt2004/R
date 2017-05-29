cc5705a0b10fc44d8a658037333087b69ced5b13
khzt2004 <khzt@hotmail.com>
2017-05-29 22:48:32 +0800

    Add files via upload

--- S&P500_yearly_returns_screener.R
+++ S&P500_yearly_returns_screener.R
@@ -1,12 +1,26 @@
 library(tidyquant)
+library(lubridate)
+library(rpivotTable)
 set.seed(10)
-sp500 <- tq_index("SP500") %>%
-  slice(1:3) %>%
+tq_get("AAPL", "key.ratios")
+sp500 <- tq_index("SP500", use_fallback = TRUE) %>%
   tq_get(get = "stock.prices")
 
+sp500_keystats <- tq_index("SP500", use_fallback = TRUE) %>%
+  tq_get(get = "key.stats")
+
+sp500_keystats_selected <- select(sp500_keystats, c(symbol, company, Market.Capitalization))
+
+# sp500_filter <- filter(sp500_keystats, symbol == "jpm")
+  
 stocks_returns_yearly <- sp500 %>%
   group_by(symbol) %>%
   tq_transmute(select     = adjusted, 
                mutate_fun = periodReturn, 
                period     = "yearly", 
-               col_rename = "yearly.returns") 
+               col_rename = "yearly.returns") %>%
+  mutate(year = year(date)) %>%
+  left_join(sp500_keystats_selected, by="symbol")
+
+
+rpivotTable(stocks_returns_yearly)
