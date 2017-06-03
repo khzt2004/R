e27fe3c94ebde528088c8a5fa94150fec3e0eafb
khzt2004 <khzt@hotmail.com>
2017-06-03 22:07:06 +0800

    Add files via upload

--- Stock Plots - Graph.R
+++ Stock Plots - Graph.R
@@ -1,18 +1,42 @@
 library(tidyquant)
 library(rpivotTable)
 
-stock_list <- tibble(stocks = c("AVGO", "COHR", "TXN", "INTC"))
+# stock_list <- tibble(stocks = c("AVGO", "COHR", "TXN", "INTC", "AMD"))
+stock_list <- tibble(stocks = c("Y45.SI", "AVV.SI", "M14.SI", "S58.SI"),
+                     company = c(
+                       "Singapore Myanmar Investco", 
+                       "CEI Ltd", 
+                       "Innotek",
+                       "SATS"))
 stock_prices <- stock_list %>%
-  tq_get(get = "stock.prices", from = "2007-01-01", to = "2017-06-01") %>%
-  filter(date > "2016-01-01")
+  tq_get(get = "stock.prices", from = "2007-01-01", to = "2017-06-01")
 
+stock_prices <- stock_prices %>%
+  filter(date > "2017-01-01" & stocks != "S58.SI")
 
+# plot stock prices
 industry_chart <- stock_prices %>%
-  ggplot(aes(x = date, y = close, color = stocks)) +
+  ggplot(aes(x = date, y = close, color = company)) +
   geom_line() +
-  labs(title = "stock Line Chart", y = "Closing Price", x = "") + 
+  labs(title = "Stock Line Chart", y = "Closing Price", x = "") + 
   theme_tq()
 
 industry_chart
+
+# get key ratios
+stock_keystats <- stock_list %>%
+  tq_get(get = "key.ratios") %>%
+  filter(section == "Valuation Ratios") %>%
+  unnest() %>%
+  filter(category == "Price to Earnings")
+
+# plot key ratio
+ratio_chart <- stock_keystats %>%
+  ggplot(aes(x = date, y = value, color = stocks)) +
+  geom_line() +
+  labs(title = "stock Line Chart", y = "P/E", x = "") + 
+  theme_tq()
+
+ratio_chart
 
 
