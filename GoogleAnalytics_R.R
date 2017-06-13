baad93a70a7bc2666a52057fffbdfec7aa529c1b
khzt2004 <khzt@hotmail.com>
2017-06-13 22:45:36 +0800

    Add files via upload

--- GoogleAnalytics_R.R
+++ GoogleAnalytics_R.R
@@ -9,7 +9,7 @@ ga_auth()
 account_list <- ga_account_list()
 
 ## pick a profile with data to query
-ga_id <- account_list[106,'viewId']
+ga_id <- account_list[92,'viewId']
 
 ## get a list of what metrics and dimensions you can use
 ga_auth()
@@ -30,18 +30,42 @@ se3 <- segment_element("medium",
                        expressions = "^(email|referral)$",
                        scope = "SESSION")
 
-sv_simple <- segment_vector_simple(list(list(se3)))
+sv_simple <- segment_vector_simple(list(list(segment_ga_google5sec)))
 seg_defined <- segment_define(sv_simple)
 segment4 <- segment_ga4("simple", user_segment = seg_defined)
 
-segment_def_for_call <- "sessions::condition::ga:medium=~^(email|referral)$"
-seg_obj <- segment_ga4("test", segment_id = segment_def_for_call)
+
+# segments: semicolon is "AND", a comma is "OR"
+
+segment_def_medium <- "sessions::condition::ga:medium=~^(email|referral)$"
+seg_obj_medium <- segment_ga4("test", segment_id = segment_def_medium)
+
+segment_def_google30sec <- "sessions::condition::ga:source=~^(google)$;ga:timeOnPage>30"
+seg_obj_google30sec <- segment_ga4("test", segment_id = segment_def_google30sec)
+
+segment_def_morethan3sessions <- "sessions::condition::ga:sessions>3"
+seg_obj_morethan3sessions <- segment_ga4("test", segment_id = segment_def_morethan3sessions)
+
+segment_def_orgtraffic_w_conversions <- "sessions::condition::ga:medium=~^(organic)$;ga:goal11Completions>0"
+seg_obj_orgtraffic_w_conversions <- segment_ga4("test", segment_id = segment_def_orgtraffic_w_conversions)
 
 segment_seq_example <- google_analytics_4(ga_id, 
                                           date_range = c("2017-01-01","2017-03-01"), 
-                                          dimensions = c('medium','country'), 
-                                          segments = seg_obj,
-                                          metrics = c('sessions','bounces')
+                                          dimensions = c('source','country'), 
+                                          segments = seg_obj_orgtraffic_w_conversions,
+                                          metrics = c('sessions','bounceRate', 'timeOnPage', 'goal11Completions')
 )
 
 segment_seq_example
+
+segment_def_mktids <- "sessions::condition::ga:dimension2=@mktid"
+seg_obj_mktids <- segment_ga4("test", segment_id = segment_def_mktids)
+
+segment_seq_mktids <- google_analytics_4(ga_id, 
+                                          date_range = c("2017-01-01","2017-03-01"), 
+                                          dimensions = c('source','dimension2'), 
+                                          segments = seg_obj_mktids,
+                                          metrics = c('sessions','bounceRate', 'timeOnPage', 'goal11Completions')
+)
+
+segment_seq_mktids
