f9b1e58c60ce89ff7ed85926e1a3130b68408a0a
khzt2004 <khzt@hotmail.com>
2017-06-15 16:09:46 +0800

    Add files via upload

--- Multi Touch Campaign Analysis.R
+++ Multi Touch Campaign Analysis.R
@@ -14,13 +14,62 @@ dfmultitouch1 <- dfmultitouch %>%
   mutate(created_day = wday(Lead.Created.Date, label = TRUE)) %>%
   mutate(makedate = as_date(Lead.Created.Date))
 
-EndAccountTest <- dfmultitouch1 %>%
-  filter(End.Account.Pseudo.ID == "ANGUSR7N")
+rpivotTable(dfmultitouch1)
 
-EndAccountTest %>%
-  ggplot(aes(x = Lead.Created.Date, color = Scorecard.Product)) + 
+# create table of lic dollars for each ID
+df_licdollars <- dfmultitouch1 %>% 
+  select(End.Account.Pseudo.ID, Lic.Dollars) %>%
+  replace_na(list(Lic.Dollars=0)) %>%
+  group_by(End.Account.Pseudo.ID) %>%
+  summarise(Lic_Dollars = sum(Lic.Dollars))
+
+rpivotTable(df_licdollars)
+
+# count of different activities for each ID
+dfmultitouch_count <- dfmultitouch1 %>%
+  select(End.Account.Pseudo.ID, Sub.Delivery.Mechanism, Lic.Dollars) %>%
+  group_by(End.Account.Pseudo.ID, Sub.Delivery.Mechanism) %>%
+  summarize(count=n()) %>%
+  spread(Sub.Delivery.Mechanism, count) %>%
+  left_join(df_licdollars, by = "End.Account.Pseudo.ID")
+
+dfmultitouch_count[is.na(dfmultitouch_count)] <- 0
+
+rpivotTable(dfmultitouch_count)
+
+# stacked bar chart for each ID 
+EndAccountTest_bar <- dfmultitouch1 %>%
+  select(End.Account.Pseudo.ID, Sub.Delivery.Mechanism, makedate, Lic.Dollars) %>%
+  group_by(End.Account.Pseudo.ID, Sub.Delivery.Mechanism, makedate) %>%
+  summarize(count=n()) %>%
+  filter(End.Account.Pseudo.ID %in% c("ANGUSR7N", "BANGKO1T", "KAOCON1T")) %>%
+  ggplot(aes(x = End.Account.Pseudo.ID, y = count, fill = Sub.Delivery.Mechanism)) +
+  geom_bar(stat = 'identity')
+
+EndAccountTest_bar
+
+# stacked bar chart for each ID by date
+EndAccountTest_bar_date <- dfmultitouch1 %>%
+  select(End.Account.Pseudo.ID, Sub.Delivery.Mechanism, makedate, Lic.Dollars) %>%
+  group_by(End.Account.Pseudo.ID, Sub.Delivery.Mechanism, makedate) %>%
+  summarize(count=n()) %>%
+  filter(End.Account.Pseudo.ID %in% c("ANGUSR7N", "BANGKO1T", "KAOCON1T")) %>%
+  ggplot(aes(x = makedate, y = count, fill = Sub.Delivery.Mechanism)) +
+  geom_bar(stat = 'identity')
+
+EndAccountTest_bar_date
+
+# line plot scorecard product breakdown by date
+EndAccountTest <- dfmultitouch1 %>%
+  select(End.Account.Pseudo.ID, Scorecard.Product, Sub.Delivery.Mechanism, makedate, Lic.Dollars) %>%
+  group_by(End.Account.Pseudo.ID, Scorecard.Product, Sub.Delivery.Mechanism, makedate) %>%
+  summarize(count=n()) %>%
+  ggplot(aes(x = makedate, color = Scorecard.Product)) + 
   geom_freqpoly(binwidth = 86400) # 86400 seconds = 1 day
 
+EndAccountTest
+
+# country table by ID
 EndAccountSummary <- dfmultitouch1 %>%
   filter(End.Account.Pseudo.ID == "ANGUSR7N") %>%
   group_by(End.Account.Pseudo.ID, Scorecard.Product, Lead.Account.Country) %>%
