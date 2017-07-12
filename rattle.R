2a00831738fadbe68161d6c62fc2e9109f595bd0
khzt2004 <khzt@hotmail.com>
2017-07-12 17:00:02 +0800

    Add files via upload

---
+++ rattle.R
@@ -0,0 +1,5 @@
+install.packages("rattle")
+install.packages("https://cran.r-project.org/bin/windows/contrib/3.3/RGtk2_2.20.31.zip", repos=NULL)
+# https://www.youtube.com/watch?v=OBilaZZpvGs
+library(rattle)
+rattle()
