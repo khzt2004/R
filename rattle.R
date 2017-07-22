97fea2a4d2570737265da96c67603d28ddea4ba3
khzt2004 <khzt@hotmail.com>
2017-07-22 21:32:09 +0800

    Add files via upload

--- rattle.R
+++ rattle.R
@@ -1,5 +1,6 @@
-install.packages("rattle")
-install.packages("https://cran.r-project.org/bin/windows/contrib/3.3/RGtk2_2.20.31.zip", repos=NULL)
-# https://www.youtube.com/watch?v=OBilaZZpvGs
+# installation instructions
+# https://gist.github.com/sebkopf/9405675
+
+library(RGtk2)
 library(rattle)
 rattle()
