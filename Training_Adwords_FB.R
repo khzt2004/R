274c71db1bc82d2158929eab21e0416f6dc81ef5
khzt2004 <khzt@hotmail.com>
2017-08-30 16:28:09 +0800

    Add files via upload

--- Training_Adwords_FB.R
+++ Training_Adwords_FB.R
@@ -3,24 +3,47 @@ library(tidyverse)
 library(viridis)
 library(ggthemes)
 library(googleAnalyticsR)
+library(lubridate)
+library(rpivotTable)
+library(TTR)
+library(googlesheets)
 
 
 ga_auth()
 ## get your accounts
-account_list <- google_analytics_account_list()
-id <- account_list[100,'viewId']
-unsampled_data_fetch <- google_analytics_4(id, date_range = c("2017-08-26", "2017-09-01"), 
+account_list <- ga_account_list()
+id <- account_list[13,'viewId']
+my_segments <- ga_segment_list()
+segs <- my_segments$items
+segment_for_call_FB <- "gaid::oskyC39UQi6wpgYOLCLMww"
+seg_obj_FB <- segment_ga4("Training Campaign_FB", segment_id = segment_for_call_FB)
+segment_for_call_Google <- "gaid::rj-Q3pVwTtyja1bubZPp8w"
+seg_obj_Google <- segment_ga4("Training Campaign_Google", segment_id = segment_for_call_Google)
+
+startDate <- "2017-08-19"
+endDate <- "2017-08-25"
+
+unsampled_data_fetch_FB <- google_analytics_4(id, date_range = c(startDate, endDate), 
                                            metrics = c("sessions", "pageViews", "entrances", "bounceRate", "avgTimeOnPage", "exitRate"), 
-                                           dimensions = c("pagePath", "dimension2", "date"),
+                                           dimensions = c("dayOfWeekName", "hour"),
+                                           segments = seg_obj_FB,
                                            anti_sample = TRUE)
 
-
+unsampled_data_fetch_Google <- google_analytics_4(id, date_range = c(startDate, endDate), 
+                                              metrics = c("sessions", "pageViews", "entrances", "bounceRate", "avgTimeOnPage", "exitRate"), 
+                                              dimensions = c("dayOfWeekName", "hour"),
+                                              segments = seg_obj_Google,
+                                              anti_sample = TRUE)
 
 # Google heatmap
 # google_heatmap <- read.csv("google_heatmap.csv", sep=",")
-# google_heatmap1 <- spread(google_heatmap, Day.of.Week.Name, Sessions, fill = 0)
-google_heatmap$Day.of.Week.Name <- factor(google_heatmap$Day.of.Week.Name, c("Sunday", "Saturday", "Friday", "Thursday", "Wednesday", "Tuesday", "Monday"))
-google_heatmap$wDay <- ifelse(google_heatmap$Day.of.Week.Name %in% c("Saturday", "Sunday"), "Weekend", "Weekday")
+# google_heatmap1 <- spread(unsampled_data_fetch_Google, dayOfWeekName, Sessions, fill = 0)
+google_heatmap <- unsampled_data_fetch_Google
+google_heatmap$dayOfWeekName <- factor(google_heatmap$dayOfWeekName, c("Sunday", "Saturday", "Friday", "Thursday", "Wednesday", "Tuesday", "Monday"))
+google_heatmap <- rename(google_heatmap, Hour = hour)
+google_heatmap$Hour <- as.numeric(google_heatmap$Hour)
+rename(df,c('foo'='samples'))
+google_heatmap$wDay <- ifelse(google_heatmap$dayOfWeekName %in% c("Saturday", "Sunday"), "Weekend", "Weekday")
 
 # use this if summarising by weekend vs weekday
 #google_heatmap <- google_heatmap %>%
@@ -28,10 +51,10 @@ google_heatmap$wDay <- ifelse(google_heatmap$Day.of.Week.Name %in% c("Saturday",
 #  summarise(Sessions = sum(Sessions))
 
 google_heatmap %>%
-  ggplot(aes(x=Hour, y=Day.of.Week.Name, fill=Sessions)) +  # change y-axis if summary by weekend vs weekday
+  ggplot(aes(x=Hour, y=dayOfWeekName, fill=sessions)) +  # change y-axis if summary by weekend vs weekday
   geom_tile(color="white", size=0.1) +
-  # scale_fill_continuous(low = "#7baaf7", high = "#3367d6") + 
-  scale_fill_viridis(name="Sessions") +
+  scale_fill_gradient(low = "#f6f6f6", high = "#3367d6") + 
+  # scale_fill_viridis(name="Sessions", option = "C") +
   scale_x_continuous(breaks = c(0:23), expand=c(0,0)) +
   scale_y_discrete(expand=c(0,0)) +
   coord_equal() +
@@ -40,15 +63,18 @@ google_heatmap %>%
   theme(axis.ticks=element_blank()) +
   theme(axis.text=element_text(size=9)) + 
   theme(legend.title=element_text(size=8)) +
-  theme(legend.text=element_text(size=6)) +
-  annotate("rect", xmin=-0.5, xmax=8, ymin=-Inf, ymax=Inf, alpha=.2, fill="blue")
+  theme(legend.text=element_text(size=6))
+  # annotate("rect", xmin=-0.5, xmax=8, ymin=-Inf, ymax=Inf, alpha=.2, fill="blue")
 
 
 # FB heatmap
 # fb_heatmap <- read.csv("fb_heatmap.csv", sep=",")
 # fb_heatmap1 <- spread(fb_heatmap, Day.of.Week.Name, Sessions, fill = 0)
-fb_heatmap$Day.of.Week.Name <- factor(fb_heatmap$Day.of.Week.Name, c("Sunday", "Saturday", "Friday", "Thursday", "Wednesday", "Tuesday", "Monday"))
-fb_heatmap$wDay <- ifelse(fb_heatmap$Day.of.Week.Name %in% c("Saturday", "Sunday"), "Weekend", "Weekday")
+fb_heatmap <- unsampled_data_fetch_FB
+fb_heatmap$dayofWeekName <- factor(fb_heatmap$dayOfWeekName, c("Sunday", "Saturday", "Friday", "Thursday", "Wednesday", "Tuesday", "Monday"))
+fb_heatmap <- rename(fb_heatmap, Hour = hour)
+fb_heatmap$Hour <- as.numeric(fb_heatmap$Hour)
+fb_heatmap$wDay <- ifelse(fb_heatmap$dayOfWeekName %in% c("Saturday", "Sunday"), "Weekend", "Weekday")
 
 # use this if summarising by weekend vs weekday
 #fb_heatmap <- fb_heatmap %>%
@@ -56,9 +82,10 @@ fb_heatmap$wDay <- ifelse(fb_heatmap$Day.of.Week.Name %in% c("Saturday", "Sunday
 #  summarise(Sessions = sum(Sessions))
 
 fb_heatmap %>%
-  ggplot(aes(x=Hour, y=Day.of.Week.Name, fill=Sessions)) +  # change y-axis if summary by weekend vs weekday
+  ggplot(aes(x=Hour, y=dayOfWeekName, fill=sessions)) +  # change y-axis if summary by weekend vs weekday
   geom_tile(color="white", size=0.1) +
-  scale_fill_viridis(name="Sessions") +
+  scale_fill_gradient(low = "#f6f6f6", high = "#3367d6") +
+  # scale_fill_viridis(name="Sessions") +
   scale_x_continuous(breaks = c(0:23), expand=c(0,0)) +
   scale_y_discrete(expand=c(0,0)) +
   coord_equal() +
@@ -67,5 +94,12 @@ fb_heatmap %>%
   theme(axis.ticks=element_blank()) +
   theme(axis.text=element_text(size=9)) + 
   theme(legend.title=element_text(size=8)) +
-  theme(legend.text=element_text(size=6)) +
-  annotate("rect", xmin=-0.5, xmax=8, ymin=-Inf, ymax=Inf, alpha=.2, fill="blue")
+  theme(legend.text=element_text(size=6))
+  # annotate("rect", xmin=-0.5, xmax=8, ymin=-Inf, ymax=Inf, alpha=.2, fill="blue")
+
+# create google sheets
+
+write_csv(fb_heatmap, "fb_heatmap.csv")
+training_ss <- gs_upload("fb_heatmap.csv")
+training_ss %>% gs_read()
+file.remove("fb_heatmap.csv")
