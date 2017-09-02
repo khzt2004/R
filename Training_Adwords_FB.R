467df58f4abea1fb9f177e56c0bc92f52937ea3f
khzt2004 <khzt@hotmail.com>
2017-09-02 18:49:24 +0800

    Add files via upload

--- Training_Adwords_FB.R
+++ Training_Adwords_FB.R
@@ -12,6 +12,7 @@ library(grid)
 library(gridExtra)
 
 
+
 ga_auth()
 ## get your accounts
 account_list <- ga_account_list()
@@ -28,46 +29,44 @@ seg_obj_Google <- segment_ga4("Training Campaign_Google", segment_id = segment_f
 segment_for_call_SpamFiltered <- "gaid::1PVeiyEpTWW8guG2ff3rjg"
 seg_obj_SpamFiltered <- segment_ga4("googleusercontent network domain", segment_id = segment_for_call_SpamFiltered)
 
-startDate <- "2017-08-19"
-endDate <- "2017-08-25"
-startDate2 <- "2017-08-12"
-endDate2 <- "2017-08-18"
+startDate <- "2017-08-26"
+endDate <- "2017-09-01"
+startDate2 <- "2017-08-19"
+endDate2 <- "2017-08-25"
 
 unsampled_data_fetch_FB <- google_analytics_4(id, date_range = c(startDate, endDate), 
-                                              metrics = c("sessions", "pageViews", "entrances", "bounceRate", "avgTimeOnPage", "exitRate"), 
+                                              metrics = c("users", "sessions", "pageViews", "entrances", "bounceRate", "avgTimeOnPage", "exitRate"), 
                                               dimensions = c("dayOfWeekName", "hour"),
                                               segments = seg_obj_FB)
 
 unsampled_data_fetch_Google <- google_analytics_4(id, date_range = c(startDate, endDate), 
-                                                  metrics = c("sessions", "pageViews", "entrances", "bounceRate", "avgTimeOnPage", "exitRate"), 
+                                                  metrics = c("users", "sessions", "pageViews", "entrances", "bounceRate", "avgTimeOnPage", "exitRate"), 
                                                   dimensions = c("dayOfWeekName", "hour"),
                                                   segments = seg_obj_Google,
                                                   anti_sample = TRUE)
 
-unsampled_data_fetch_campaigns <- google_analytics_4(id, date_range = c(startDate, endDate), 
+unsampled_data_fetch_campaigns <- google_analytics_4(id, date_range = c(startDate, endDate, startDate2, endDate2), 
                                                      metrics = c("users", "newUsers", "sessions", "bounceRate", "pageviewsPerSession", "entrances", "avgTimeOnPage", "exitRate", "avgSessionDuration", "transactionsPerSession"), 
                                                      dimensions = c("Campaign", "adGroup"),
-                                                     segments = c(seg_obj_Google, seg_obj_FB),
-                                                     anti_sample = TRUE)
+                                                     segments = c(seg_obj_Google, seg_obj_FB))
 
-unsampled_data_fetch_adwordcampaigns <- google_analytics_4(id, date_range = c(startDate, endDate), 
+unsampled_data_fetch_adwordcampaigns <- google_analytics_4(id, date_range = c(startDate, endDate, startDate2, endDate2), 
                                                      metrics = c("impressions", "adClicks", "adCost", "CTR", "CPC"), 
-                                                     dimensions = c("Campaign", "adGroup"),
-                                                     anti_sample = TRUE)
+                                                     dimensions = c("Campaign", "adGroup"))
 
 adwordsCombined <- unsampled_data_fetch_campaigns %>%
   left_join(unsampled_data_fetch_adwordcampaigns, by = "adGroup") %>%
   select(-c(Campaign.x, Campaign.y)) %>%
   filter(segment == "Training Campaign_Google") %>%
   select(-segment) %>%
-  gather(metrics, values, 2:16) %>% 
+  gather(metrics, values, 2:31) %>% 
   spread(adGroup, values)
+  
 
 adwordsCombined[, 2:5] <- round(adwordsCombined[, 2:5], 2)
+adwordsCombined = transform(adwordsCombined, metrics = colsplit(metrics, "\\.", names = c('metric', 'dateRange')))
 
-
-  
-unsampled_data_fetch_deviceLP <- google_analytics_4(id, date_range = c(startDate, endDate, startDate2, endDate2), 
+unsampled_data_fetch_deviceLP <- google_analytics_4(id, date_range = c(startDate, endDate), 
                                                     metrics = c("users", "newUsers", "sessions", "bounceRate", "pageviewsPerSession", "entrances", "avgTimeOnPage", "exitRate", "avgSessionDuration", "transactionsPerSession"), 
                                                     dimensions = c("deviceCategory", "landingPagePath"),
                                                     segments = c(seg_obj_Google, seg_obj_FB))
@@ -152,7 +151,17 @@ fb_heatmap %>%
 
 # create google sheets
 
-write_csv(fb_heatmap, "fb_heatmap.csv")
-training_ss <- gs_upload("fb_heatmap.csv")
-training_ss %>% gs_read()
-file.remove("fb_heatmap.csv")
+write_csv(unsampled_data_fetch_device_AdKeyword, "unsampled_data_fetch_device_AdKeyword.csv")
+keyword_ss <- gs_upload("unsampled_data_fetch_device_AdKeyword.csv")
+keyword_ss %>% gs_read()
+file.remove("unsampled_data_fetch_device_AdKeyword.csv")
+
+write_csv(unsampled_data_fetch_deviceLP, "unsampled_data_fetch_deviceLP.csv")
+deviceLP_ss <- gs_upload("unsampled_data_fetch_deviceLP.csv")
+deviceLP_ss %>% gs_read()
+file.remove("unsampled_data_fetch_deviceLP.csv")
+
+write_csv(adwordsCombined, "adwordsCombined.csv")
+adwordsCombined_ss <- gs_upload("adwordsCombined.csv")
+adwordsCombined_ss %>% gs_read()
+file.remove("adwordsCombined.csv")
