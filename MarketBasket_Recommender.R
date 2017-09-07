9d245b992541eef217fe95649e57376d0080d401
khzt2004 <khzt@hotmail.com>
2017-09-07 17:47:50 +0800

    Add files via upload

--- MarketBasket_Recommender.R
+++ MarketBasket_Recommender.R
@@ -10,6 +10,7 @@
 # https://rpubs.com/cheverne/associationrules_marketbasketanalysis
 # https://rstudio-pubs-static.s3.amazonaws.com/267119_9a033b870b9641198b19134b7e61fe56.html -> ECLAT
 # https://benjnmoss.wordpress.com/2017/02/13/market-basket-analysis-in-alteryx/
+# https://synerise.com/data-mining-how-to-analyze-customers-market-baskets-to-increase-sales/#
 
 library(arules)  # association rules
 library(arulesViz)  # data visualization of association rules
@@ -17,6 +18,7 @@ library(RColorBrewer)  # color palettes for plots
 library(tidyverse)
 library(lubridate)
 
+# read sample data into dataframe
 raw_data <- read_csv("https://raw.githubusercontent.com/kh7393/Market-Basket/master/Online%20Retail_new.csv")
 raw_data <- raw_data %>%
   mutate(InvoiceDate = format(InvoiceDate, "%H:%M:%S"))
@@ -27,15 +29,16 @@ raw_data1 <- raw_data %>% select(Country, InvoiceNo) %>%
   group_by(Country) %>%
   summarize(InvoiceNo, n())
 
-
 # Remove the canceled/refunded orders 
 # Remove rows with invalid product description 
 
-# select the rows with relevant data and convert them to S4 object
-
+# select the rows with relevant data for analysis
 transaction_detail <- aggregate(raw_data$AirlineDescription ~ raw_data$InvoiceNo,
                                 FUN=paste,collapse=',')
+# remove column for invoice number
 transaction_itemsets<-transaction_detail[,-1]
+
+# convert to transactions object for market basket analysis
 write(transaction_itemsets,"itemsets2.csv")
 itemsets_txn<-read.transactions("itemsets2.csv",format="basket",rm.duplicates=TRUE,sep=",")
 
@@ -43,7 +46,7 @@ itemsets_txn<-read.transactions("itemsets2.csv",format="basket",rm.duplicates=TR
 print(dim(itemsets_txn))
 
 print(dim(itemsets_txn)[1])  # X no. market baskets for flight trips
-print(dim(itemsets_txn)[2])  # X no. of initial store items
+print(dim(itemsets_txn)[2])  # X no. of initial product/items
 
 # summary of dataset including most frequent items, itemset/transaction length distribution
 summary(itemsets_txn)
@@ -51,7 +54,7 @@ summary(itemsets_txn)
 # find the top 15 items
 itemFrequencyPlot(itemsets_txn, topN=15)
 
-# examine frequency for each item with support greater than 0.025
+# exploratory plotting - examine frequency for each item with support greater than 0.025
 pdf(file="fig_market_basket_initial_item_support.pdf", 
     width = 8.5, height = 11)
 itemFrequencyPlot(itemsets_txn, support = 0.025, cex.names=0.8, xlim = c(0,0.3),
@@ -60,17 +63,6 @@ itemFrequencyPlot(itemsets_txn, support = 0.025, cex.names=0.8, xlim = c(0,0.3),
                                "\n(Item Relative Frequency or Support)"))
 dev.off()    
 
-# explore possibilities for combining similar items
-print(head(itemInfo(itemsets_txn))) 
-print(levels(itemInfo(itemsets_txn)[["level1"]]))  # 10 levels... too few 
-print(levels(itemInfo(itemsets_txn)[["level2"]]))  # 55 distinct levels
-
-# aggregate items using the 55 level2 levels for food categories
-# to create a more meaningful set of items
-itemsets_txn1 <- aggregate(itemsets_txn, itemInfo(itemsets_txn)[["level2"]])  
-
-print(dim(groceries)[1])  # 9835 market baskets for shopping trips
-print(dim(groceries)[2])  # 55 final store items (categories)  
 
 pdf(file="fig_market_basket_final_item_support.pdf", width = 8.5, height = 11)
 itemFrequencyPlot(itemsets_txn, support = 0.025, cex.names=1.0, xlim = c(0,0.5),
@@ -78,8 +70,6 @@ itemFrequencyPlot(itemsets_txn, support = 0.025, cex.names=1.0, xlim = c(0,0.5),
                   xlab = paste("Proportion of Market Baskets Containing Item",
                                "\n(Item Relative Frequency or Support)"))
 dev.off()   
-
-
 
 # obtain large set of association rules for items by category and all shoppers
 # this is done by setting very low criteria for support and confidence
@@ -114,21 +104,14 @@ plot(second.rules, method="grouped",
      control=list(col = rev(brewer.pal(9, "Greens")[4:9])))
 # dev.off()    
 
-# select rules with vegetables in consequent (right-hand-side) item subsets
-vegie.rules <- subset(second.rules, subset = rhs %pin% "vegetables")
-inspect(vegie.rules)  # 41 rules
-
-# sort by lift and identify the top 10 rules
-top.vegie.rules <- head(sort(vegie.rules, decreasing = TRUE, by = "lift"), 10)
-inspect(top.vegie.rules) 
-
+# this needs fixing
 pdf(file="fig_market_basket_farmer_rules.pdf", width = 11, height = 8.5)
-plot(top.vegie.rules, method="graph", 
+plot(top.second.rules, method="graph", 
      control=list(type="items"), 
      shading = "lift")
 dev.off()  
 
-plot(rules,method="graph",interactive=TRUE,shading=NA)
+# plot(second.rules,method="graph",interactive=TRUE,shading=NA)
 
 itemsets_txnrules_df <- as(second.rules, "data.frame")
 itemsets_txnrules_df <- itemsets_txnrules_df %>%
@@ -136,9 +119,11 @@ itemsets_txnrules_df <- itemsets_txnrules_df %>%
   mutate(InverseConfidence = (support * lift) / confidence)
 
 # final table of recommended rules to use filtered by max confidence
+# option to sort by lift
 # if LHS is bought then RHS is purchased
 rules_final <- itemsets_txnrules_df %>%
-  filter(confidence > InverseConfidence)
+  filter(confidence > InverseConfidence) # %>%
+  # arrange(desc(lift))
 
 # non-case sensitive filter
 filteredrules_df <- rules_final %>%
