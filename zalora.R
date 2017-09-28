7778e9d1d1b0dafe6e60da8e6ba8062331c8780f
khzt2004 <khzt@hotmail.com>
2017-09-28 17:41:53 +0800

    Add files via upload

--- zalora.R
+++ zalora.R
@@ -3,28 +3,30 @@ library(tidyverse)
 library(stringr)
 library(googlesheets)
 library(openxlsx)
+library(readxl)
 
-zalora <- read_xlsx('C:/Users/User/Desktop/Zalora/Zalora Week 2 Data_test.xlsx', sheet = 'By Completion Rate')
+zalora <- read_xlsx('C:/Users/User/Desktop/Zalora/Zalora Week 3 Data.xlsx', sheet = 'By Completion Rate')
 zalora <- zalora %>%
   spread(Progress,`Number of Users`) %>%
   mutate(key = paste(`Device Type`, Region, `Clip Name`)) %>%
-  select(1:4, 10, 5:9) %>%
+  select(1:3, 9, 4:8) %>%
   rename(`0%` =`0`,
          `25%` = `0.25`,
          `50%` = `0.5`,
          `75%` = `0.75`,
          `100%` = `1`)
 
-zalora_product <- read_xlsx('C:/Users/User/Desktop/Zalora/Zalora Week 2 Data_test.xlsx', sheet = 'By Product')
+# need to re-write this portion. select columns 1:4, sum product views and buy clicks, group by 1:4
+zalora_product <- read_xlsx('C:/Users/User/Desktop/Zalora/Zalora Week 3 Data.xlsx', sheet = 'By Product')
 zalora_product <- zalora_product %>%
   mutate(key = paste(`Device Type`, Region, `Clip Name`)) %>%
   select(1:4, key, 5:14) %>%
-  left_join(zalora[5:10], by = "key" )
+  left_join(zalora[4:9], by = "key" )
 
-wb <- loadWorkbook('C:/Users/User/Desktop/Zalora/Zalora Week 2 Data_test.xlsx')
+wb <- loadWorkbook('C:/Users/User/Desktop/Zalora/Zalora Week 3 Data.xlsx')
 addWorksheet(wb, 'By Completion Rate 3', gridLines = FALSE)
 addWorksheet(wb, 'By Product 1', gridLines = FALSE)
 writeData(wb, sheet = 'By Completion Rate 3', zalora)
 writeData(wb, sheet = 'By Product 1', zalora_product)
-saveWorkbook(wb,'C:/Users/User/Desktop/Zalora/Zalora Week 2 Data_test.xlsx',overwrite = T)
+saveWorkbook(wb,'C:/Users/User/Desktop/Zalora/Zalora Week 3 Data.xlsx',overwrite = T)
 
