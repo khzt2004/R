1fc288cb5309e0705359340a13b6f2a012ab9d55
khzt2004 <khzt@hotmail.com>
2017-10-04 14:54:09 +0800

    Add files via upload

--- Zalora_Sheets.R
+++ Zalora_Sheets.R
@@ -14,6 +14,9 @@ id_my <- account_list[212,'viewId']
 id_ph <- account_list[213,'viewId']
 id_sg <- account_list[214,'viewId']
 id_tw <- account_list[215,'viewId']
+
+id_combined <- c(id_hk,id_indo, id_my, id_ph, id_sg, id_tw)
+
 #my_segments <- ga_segment_list()
 #segs <- my_segments$items
 HDILASegment <- "gaid::1NZ_PZZyRnmbDSnrQ6-dgA"
@@ -25,108 +28,37 @@ seg_allUsers <- segment_ga4("All Users", segment_id = segment_for_allusers)
 startDate <- "2017-09-10"
 endDate <- "2017-09-30"
 
-#HK
-HDILA_Table_HK <- google_analytics_4(id_hk, date_range = c(startDate, endDate), 
-                                         metrics = c("sessions", "transactions", "itemRevenue"), 
-                                         dimensions = c("deviceCategory"),
-                                         segments = c(seg_HDILA, seg_allUsers),
-                                         anti_sample = TRUE)
+ga_data_merged <- data.frame()
 
-HDILA_Table_HK <- HDILA_Table_HK %>%
-  mutate(Property = "HK") %>%
-  select(6, 2:5) %>%
-  group_by(Property, segment) %>%
+for (i in id_combined) {
+  ga_data_temp <- 
+    google_analytics_4(i, #=This is a (dynamic) ViewID parameter
+                       date_range = c(startDate, endDate), 
+                       metrics = c("sessions", "transactions", "itemRevenue"), 
+                       dimensions = c("deviceCategory"),
+                       segments = c(seg_HDILA, seg_allUsers),
+                       anti_sample = TRUE,
+                       max = -1)
+  ga_data_temp$id_combined <- i
+  ga_data_merged <- rbind(ga_data_merged, ga_data_temp)
+}
+
+
+ga_data_merged <- ga_data_merged %>%
+  left_join(account_list[c("viewId", "viewName")], by = c("id_combined" = "viewId")) %>%
+  group_by(viewName, segment) %>%
   summarize(Sessions = sum(sessions), 
             Transactions = sum(transactions),
             Revenue = sum(itemRevenue)) %>%
-  arrange(desc(segment))
-# Indo
-HDILA_Table_Indo <- google_analytics_4(id_indo, date_range = c(startDate, endDate), 
-                                     metrics = c("sessions", "transactions", "itemRevenue"), 
-                                     dimensions = c("deviceCategory"),
-                                     segments = c(seg_HDILA, seg_allUsers),
+  arrange(desc(viewName, segment))
+
+# SKU/product report
+SKU_Table_HK <- google_analytics_4(id_hk, date_range = c(startDate, endDate), 
+                                     metrics = c("itemsPerPurchase", "itemRevenue"), 
+                                     dimensions = c("date", "userType", "source", "medium", "transactionId", "adContent", "productSku"),
+                                     segments = c(seg_allUsers),
+                                   filtersExpression = c("ga:campaign=~TheTake"),
                                      anti_sample = TRUE)
-
-HDILA_Table_Indo <- HDILA_Table_Indo %>%
-  mutate(Property = "Indo") %>%
-  select(6, 2:5) %>%
-  group_by(Property, segment) %>%
-  summarize(Sessions = sum(sessions), 
-            Transactions = sum(transactions),
-            Revenue = sum(itemRevenue)) %>%
-  arrange(desc(segment))
-
-# MY
-HDILA_Table_MY <- google_analytics_4(id_my, date_range = c(startDate, endDate), 
-                                     metrics = c("sessions", "transactions", "itemRevenue"), 
-                                     dimensions = c("deviceCategory"),
-                                     segments = c(seg_HDILA, seg_allUsers),
-                                     anti_sample = TRUE)
-
-HDILA_Table_MY <- HDILA_Table_MY %>%
-  mutate(Property = "MY") %>%
-  select(6, 2:5) %>%
-  group_by(Property, segment) %>%
-  summarize(Sessions = sum(sessions), 
-            Transactions = sum(transactions),
-            Revenue = sum(itemRevenue)) %>%
-  arrange(desc(segment))
-# PH
-HDILA_Table_PH <- google_analytics_4(id_ph, date_range = c(startDate, endDate), 
-                                     metrics = c("sessions", "transactions", "itemRevenue"), 
-                                     dimensions = c("deviceCategory"),
-                                     segments = c(seg_HDILA, seg_allUsers),
-                                     anti_sample = TRUE)
-
-HDILA_Table_PH <- HDILA_Table_PH %>%
-  mutate(Property = "PH") %>%
-  select(6, 2:5) %>%
-  group_by(Property, segment) %>%
-  summarize(Sessions = sum(sessions), 
-            Transactions = sum(transactions),
-            Revenue = sum(itemRevenue)) %>%
-  arrange(desc(segment))
-
-# SG
-HDILA_Table_SG <- google_analytics_4(id_sg, date_range = c(startDate, endDate), 
-                                     metrics = c("sessions", "transactions", "itemRevenue"), 
-                                     dimensions = c("deviceCategory"),
-                                     segments = c(seg_HDILA, seg_allUsers),
-                                     anti_sample = TRUE)
-
-HDILA_Table_SG <- HDILA_Table_SG %>%
-  mutate(Property = "SG") %>%
-  select(6, 2:5) %>%
-  group_by(Property, segment) %>%
-  summarize(Sessions = sum(sessions), 
-            Transactions = sum(transactions),
-            Revenue = sum(itemRevenue)) %>%
-  arrange(desc(segment))
-
-# TW
-HDILA_Table_TW <- google_analytics_4(id_tw, date_range = c(startDate, endDate), 
-                                     metrics = c("sessions", "transactions", "itemRevenue"), 
-                                     dimensions = c("deviceCategory"),
-                                     segments = c(seg_HDILA, seg_allUsers),
-                                     anti_sample = TRUE)
-
-HDILA_Table_TW <- HDILA_Table_TW %>%
-  mutate(Property = "TW") %>%
-  select(6, 2:5) %>%
-  group_by(Property, segment) %>%
-  summarize(Sessions = sum(sessions), 
-            Transactions = sum(transactions),
-            Revenue = sum(itemRevenue)) %>%
-  arrange(desc(segment))
-
-Merged_Countries <- rbind(HDILA_Table_HK,
-                          HDILA_Table_Indo,
-                          HDILA_Table_MY,
-                          HDILA_Table_PH,
-                          HDILA_Table_SG,
-                          HDILA_Table_TW)
-
-
 
 
 
