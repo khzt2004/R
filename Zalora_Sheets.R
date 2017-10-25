edef2ffcd546e5e7033f178689494889e8fdf183
khzt2004 <khzt@hotmail.com>
2017-10-25 15:04:50 +0800

    Add files via upload

--- Zalora_Sheets.R
+++ Zalora_Sheets.R
@@ -45,8 +45,8 @@ segment_for_allusers <- "gaid::-1"
 seg_allUsers <- segment_ga4("All Users", segment_id = segment_for_allusers)
 
 # enter start date and end date here. Format: yyyy-mm-dd
-startDate <- "2017-10-09"
-endDate <- "2017-10-15"
+startDate <- "2017-09-08"
+endDate <- "2017-10-22"
 
 # Traffic Report
 ga_traffic_data_merged <- data.frame()
@@ -91,10 +91,10 @@ ga_traffic_data_merged <- ga_traffic_data_merged %>%
                           ))
 
 # export dataframe as csv to your working directory
-write_csv(ga_traffic_data_merged, "traffic_wk5.csv")
+write_csv(ga_traffic_data_merged, "traffic_wk7.csv")
 
 
-# App Traffic Report - ID data is sampled, why
+# App Traffic Report
 ga_app_traffic_data_merged <- data.frame()
 
 for (i in id_app_combined) {
@@ -102,8 +102,7 @@ for (i in id_app_combined) {
     google_analytics_4(i, #=This is a (dynamic) ViewID parameter
                        date_range = c(startDate, endDate), 
                        metrics = c("sessions", "users"), 
-                       dimensions = c("deviceCategory", "sourceMedium", "date", "campaign"),
-                       segments = c(seg_allUsers),
+                       dimensions = c("deviceCategory", "sourceMedium", "date"),
                        anti_sample = TRUE,
                        max = -1)
   ga_data_temp3$id_combined <- i
@@ -137,7 +136,54 @@ ga_app_traffic_data_merged <- ga_app_traffic_data_merged %>%
   ))
 
 # export dataframe as csv to your working directory
-write_csv(ga_app_traffic_data_merged, "traffic_wk5.csv")
+write_csv(ga_app_traffic_data_merged, "app_traffic_wk7.csv")
+
+
+# App Traffic Report
+ga_app_trafficfilter_data_merged <- data.frame()
+
+for (i in id_app_combined) {
+  ga_data_temp4 <- 
+    google_analytics_4(i, #=This is a (dynamic) ViewID parameter
+                       date_range = c(startDate, endDate), 
+                       metrics = c("sessions", "users"), 
+                       dimensions = c("deviceCategory", "sourceMedium", "date", "landingScreenName"),
+                       anti_sample = TRUE,
+                       max = -1)
+  ga_data_temp4$id_combined <- i
+  ga_app_trafficfilter_data_merged <- rbind(ga_app_trafficfilter_data_merged, ga_data_temp4)
+}
+
+ga_app_trafficfilter_data_merged <- ga_app_trafficfilter_data_merged %>%
+  left_join(account_list[c("viewId", "viewName")], by = c("id_combined" = "viewId")) %>%
+  filter(grepl("HDILA", landingScreenName, ignore.case = TRUE)) %>%
+  mutate(Country = case_when(grepl("HK", viewName, ignore.case = TRUE) ~"HK",
+                             grepl("ID", viewName, ignore.case = TRUE) ~"ID",
+                             grepl("SG", viewName, ignore.case = TRUE) ~"SG",
+                             grepl("MY", viewName, ignore.case = TRUE) ~"MY",
+                             grepl("PH", viewName, ignore.case = TRUE) ~"PH",
+                             grepl("TW", viewName, ignore.case = TRUE) ~"TW"),
+         date = ymd(date)) %>%
+  mutate(Week = case_when(date >= '2017-09-08' & date <= '2017-09-10' ~ "1",
+                          date >= '2017-09-11' & date <= '2017-09-17' ~ "2",
+                          date >= '2017-09-18' & date <= '2017-09-24' ~ "3",
+                          date >= '2017-09-25' & date <= '2017-10-01' ~ "4",
+                          date >= '2017-10-02' & date <= '2017-10-08' ~ "5",
+                          date >= '2017-10-09' & date <= '2017-10-15' ~ "6",
+                          date >= '2017-10-16' & date <= '2017-10-22' ~ "7",
+                          date >= '2017-10-23' & date <= '2017-10-29' ~ "8",
+                          date >= '2017-10-30' & date <= '2017-11-05' ~ "9",
+                          date >= '2017-11-06' & date <= '2017-11-12' ~ "10",
+                          date >= '2017-11-13' & date <= '2017-11-19' ~ "11",
+                          date >= '2017-11-20' & date <= '2017-11-26' ~ "12",
+                          date >= '2017-11-27' & date <= '2017-12-03' ~ "13",
+                          date >= '2017-12-04' & date <= '2017-12-10' ~ "14",
+                          date >= '2017-12-11' & date <= '2017-12-17' ~ "15"
+  ))
+
+# export dataframe as csv to your working directory
+write_csv(ga_app_trafficfilter_data_merged, "app_traffic_filter_wk7.csv")
+
 
 # Clicked Add to Cart report
 ga_addCart_data_merged <- data.frame()
@@ -186,7 +232,55 @@ ga_addCart_data_merged <- ga_addCart_data_merged %>%
   ))
 
 # export dataframe as csv to your working directory
-write_csv(ga_addCart_data_merged, "addcart_wk5.csv")
+write_csv(ga_addCart_data_merged, "addcart_wk7.csv")
+
+
+# Clicked Add to Cart report - APP
+ga_addCart_app_data_merged <- data.frame()
+
+for (i in id_app_combined) {
+  ga_data_temp6 <- 
+    google_analytics_4(i, #=This is a (dynamic) ViewID parameter
+                       date_range = c(startDate, endDate), 
+                       metrics = c("sessions"), 
+                       dimensions = c("deviceCategory", "sourceMedium", "date", "shoppingStage", "landingScreenName"),
+                       anti_sample = TRUE,
+                       max = -1)
+  ga_data_temp6$id_combined <- i
+  ga_addCart_app_data_merged <- rbind(ga_addCart_app_data_merged, ga_data_temp6)
+}
+
+ga_addCart_app_data_merged <- ga_addCart_app_data_merged %>%
+  left_join(account_list[c("viewId", "viewName")], by = c("id_combined" = "viewId")) %>%
+  filter(grepl("HDILA", landingScreenName, ignore.case = TRUE) & 
+           grepl("^ADD_TO_CART$", shoppingStage, ignore.case = TRUE)) %>%
+  mutate(Country = case_when(grepl("HK", viewName, ignore.case = TRUE) ~"HK",
+                             grepl("ID", viewName, ignore.case = TRUE) ~"ID",
+                             grepl("SG", viewName, ignore.case = TRUE) ~"SG",
+                             grepl("MY", viewName, ignore.case = TRUE) ~"MY",
+                             grepl("PH", viewName, ignore.case = TRUE) ~"PH",
+                             grepl("TW", viewName, ignore.case = TRUE) ~"TW"),
+         date = ymd(date)) %>%
+  mutate(Week = case_when(date >= '2017-09-08' & date <= '2017-09-10' ~ "1",
+                          date >= '2017-09-11' & date <= '2017-09-17' ~ "2",
+                          date >= '2017-09-18' & date <= '2017-09-24' ~ "3",
+                          date >= '2017-09-25' & date <= '2017-10-01' ~ "4",
+                          date >= '2017-10-02' & date <= '2017-10-08' ~ "5",
+                          date >= '2017-10-09' & date <= '2017-10-15' ~ "6",
+                          date >= '2017-10-16' & date <= '2017-10-22' ~ "7",
+                          date >= '2017-10-23' & date <= '2017-10-29' ~ "8",
+                          date >= '2017-10-30' & date <= '2017-11-05' ~ "9",
+                          date >= '2017-11-06' & date <= '2017-11-12' ~ "10",
+                          date >= '2017-11-13' & date <= '2017-11-19' ~ "11",
+                          date >= '2017-11-20' & date <= '2017-11-26' ~ "12",
+                          date >= '2017-11-27' & date <= '2017-12-03' ~ "13",
+                          date >= '2017-12-04' & date <= '2017-12-10' ~ "14",
+                          date >= '2017-12-11' & date <= '2017-12-17' ~ "15"
+  ))
+
+# export dataframe as csv to your working directory
+write_csv(ga_addCart_app_data_merged, "addcart_app_wk7.csv")
+
 
 #Completed purchase report
 ga_data_completedpurchase <- data.frame()
@@ -233,72 +327,60 @@ ga_data_completedpurchase <- ga_data_completedpurchase %>%
   ))
 
 # export dataframe as csv to your working directory
-write_csv(ga_data_completedpurchase, "week5_purchase.csv")
+write_csv(ga_data_completedpurchase, "week7_purchase.csv")
 
 
+#Completed purchase report - APP
+ga_data_app_completedpurchase <- data.frame()
 
-# top of funnel report - not required
-ga_data_merged_topFunnel <- data.frame()
-
-for (i in id_combined) {
-  ga_data_temp2 <- 
+for (i in id_app_combined) {
+  ga_data_temp5 <- 
     google_analytics_4(i, #=This is a (dynamic) ViewID parameter
                        date_range = c(startDate, endDate), 
-                       metrics = c("sessions", "users"), 
-                       dimensions = c("deviceCategory", "source","medium", "channelGrouping"),
-                       segments = c(seg_allUsers),
+                       metrics = c("itemRevenue"), 
+                       dimensions = c("transactionId", "deviceCategory", "date", "productSKU"),
                        anti_sample = TRUE,
                        max = -1)
-  ga_data_temp2$id_combined <- i
-  ga_data_merged_topFunnel <- rbind(ga_data_merged_topFunnel, ga_data_temp2)
+  ga_data_temp5$id_combined <- i
+  ga_data_app_completedpurchase <- rbind(ga_data_app_completedpurchase, ga_data_temp5)
 }
 
-patterns <- c("Email", "Organic", 
-  "Retargeting", "Affiliate", 
-  "cpc", "Onsite", "Referring Source - Diva")
-
-ga_data_merged_topFunnel <- ga_data_merged_topFunnel %>%
+ga_data_app_completedpurchase <- ga_data_app_completedpurchase %>%
   left_join(account_list[c("viewId", "viewName")], by = c("id_combined" = "viewId")) %>%
-  select(-id_combined, -segment) %>%
-  mutate(clean1 = case_when(source == '(direct)' ~ "Onsite",
-                            source == 'Affiliate' ~ "Affiliate",
-                            grepl('diva, ', source, ignore.case = TRUE) ~"Referring Source - Diva",
-                            TRUE ~ medium)) %>%
-  mutate(clean2 = case_when(channelGrouping == 'Social' ~ "Social",
-                            TRUE ~ clean1)) %>%
-  filter(grepl(paste(patterns, collapse = "|"),clean2, ignore.case = TRUE))
- 
+  mutate(Country = case_when(grepl("HK", viewName, ignore.case = TRUE) ~"HK",
+                             grepl("ID", viewName, ignore.case = TRUE) ~"ID",
+                             grepl("SG", viewName, ignore.case = TRUE) ~"SG",
+                             grepl("MY", viewName, ignore.case = TRUE) ~"MY",
+                             grepl("PH", viewName, ignore.case = TRUE) ~"PH",
+                             grepl("TW", viewName, ignore.case = TRUE) ~"TW"),
+         date = ymd(date)) %>%
+  mutate(Week = case_when(date >= '2017-09-08' & date <= '2017-09-10' ~ "1",
+                          date >= '2017-09-11' & date <= '2017-09-17' ~ "2",
+                          date >= '2017-09-18' & date <= '2017-09-24' ~ "3",
+                          date >= '2017-09-25' & date <= '2017-10-01' ~ "4",
+                          date >= '2017-10-02' & date <= '2017-10-08' ~ "5",
+                          date >= '2017-10-09' & date <= '2017-10-15' ~ "6",
+                          date >= '2017-10-16' & date <= '2017-10-22' ~ "7",
+                          date >= '2017-10-23' & date <= '2017-10-29' ~ "8",
+                          date >= '2017-10-30' & date <= '2017-11-05' ~ "9",
+                          date >= '2017-11-06' & date <= '2017-11-12' ~ "10",
+                          date >= '2017-11-13' & date <= '2017-11-19' ~ "11",
+                          date >= '2017-11-20' & date <= '2017-11-26' ~ "12",
+                          date >= '2017-11-27' & date <= '2017-12-03' ~ "13",
+                          date >= '2017-12-04' & date <= '2017-12-10' ~ "14",
+                          date >= '2017-12-11' & date <= '2017-12-17' ~ "15"
+  ))
 
-ga_data_merged_topFunnel_sessions <- ga_data_merged_topFunnel %>%
-  select(deviceCategory, sessions, clean2)
-
-ga_data_merged_topFunnel_sessions <- ga_data_merged_topFunnel_sessions %>%
-  mutate(row = 1:nrow(ga_data_merged_topFunnel_sessions)) %>%
-  spread(clean2, sessions) %>%
-  group_by(deviceCategory) %>%
-  summarize_all(sum, na.rm = TRUE) %>%
-  mutate(type = "sessions") %>%
+ga_data_app_completedpurchase1 <- ga_data_app_completedpurchase %>%
+  select(Country, Week, deviceCategory, transactionId, productSKU, itemRevenue) %>%
+  group_by(Country, Week, deviceCategory) %>%
   rowwise() %>%
-  mutate(Email = sum(email, Email),
-         Retargeting = sum(retargeting, Retargeting)) %>%
-  select(1, 12,6,8,11,3,4,7,9)
+  mutate(transactionCount = n_distinct(transactionId),
+         productSKUCount = count(productSKU),
+         revenueSum = sum(itemRevenue))
 
-ga_data_merged_topFunnel_users <- ga_data_merged_topFunnel %>%
-  select(deviceCategory, users, clean2)
+# export dataframe as csv to your working directory
+write_csv(ga_data_app_completedpurchase, "week7_app_purchase.csv")
 
-ga_data_merged_topFunnel_users <- ga_data_merged_topFunnel_users %>%
-  mutate(row = 1:nrow(ga_data_merged_topFunnel_users)) %>%
-  spread(clean2, users) %>%
-  group_by(deviceCategory) %>%
-  summarize_all(sum, na.rm = TRUE) %>%
-  mutate(type = "users") %>%
-  rowwise() %>%
-  mutate(Email = sum(email, Email),
-         Retargeting = sum(retargeting, Retargeting)) %>%
-  select(1, 12,6,8,11,3,4,7,9)
 
-ga_data_merged_topFunnel_users_Sessions <- rbind(ga_data_merged_topFunnel_users, ga_data_merged_topFunnel_sessions)
-
-# export csv to your working directory
-write_csv(ga_data_merged_topFunnel, "funnel_wkX.csv")
 
