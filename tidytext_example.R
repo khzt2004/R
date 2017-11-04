ca01c3b5a731788fce7478d20381392314f3b8f0
khzt2004 <khzt@hotmail.com>
2017-11-04 17:44:16 +0800

    Add files via upload

--- tidytext_example.R
+++ tidytext_example.R
@@ -2,6 +2,11 @@ library(tidytext)
 library(janeaustenr)
 library(stringr)
 library(tidyverse)
+library(gutenbergr)
+library(scales)
+
+hgwells <- gutenberg_download(c(35, 36, 5230, 159))
+bronte <- gutenberg_download(c(1260, 768, 969, 9182, 767))
 
 original_books <- austen_books() %>%
   group_by(book) %>%
@@ -10,5 +15,71 @@ original_books <- austen_books() %>%
                                                  ignore_case = TRUE)))) %>%
   ungroup()
 
+# tokenize, filter out stopwords
+data(stop_words)
 tidy_books <- original_books %>%
-  unnest_tokens(word, text)
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words) 
+
+
+# plot word counts 
+tidy_books %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 600) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+# HG wells from Project Gutenberg
+tidy_hgwells <- hgwells %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+
+# Bronte from Gutenberg
+tidy_bronte <- bronte %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+
+
+frequency <- bind_rows(mutate(tidy_bronte, author = "Brontë Sisters"),
+                       mutate(tidy_hgwells, author = "H.G. Wells"), 
+                       mutate(tidy_books, author = "Jane Austen")) %>% 
+  mutate(word = str_extract(word, "[a-z']+")) %>%
+  count(author, word) %>%
+  group_by(author) %>%
+  mutate(proportion = n / sum(n)) %>% 
+  select(-n) %>% 
+  spread(author, proportion) %>% 
+  gather(author, proportion, `Brontë Sisters`:`H.G. Wells`)
+
+# expect a warning about rows with missing values being removed
+ggplot(frequency, aes(x = proportion, y = `Jane Austen`, color = abs(`Jane Austen` - proportion))) +
+  geom_abline(color = "gray40", lty = 2) +
+  geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+  geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+  scale_x_log10(labels = percent_format()) +
+  scale_y_log10(labels = percent_format()) +
+  scale_color_gradient(limits = c(0, 0.001), low = "darkslategray4", high = "gray75") +
+  facet_wrap(~author, ncol = 2) +
+  theme(legend.position="none") +
+  labs(y = "Jane Austen", x = NULL)
+
+# correlation of word frequencies
+# the word frequencies are more correlated 
+# between the Austen and Brontë novels than between Austen and H.G. Wells.
+cor.test(data = frequency[frequency$author == "Brontë Sisters",],
+         ~ proportion + `Jane Austen`)
+
+cor.test(data = frequency[frequency$author == "H.G. Wells",], 
+         ~ proportion + `Jane Austen`)
+
+
+
+
+
+
+
+
+
