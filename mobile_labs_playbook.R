c118250636b9e786a27bd570e586e92a8e0bc914
khzt2004 <khzt@hotmail.com>
2018-01-18 13:54:31 +0800

    Add files via upload

--- mobile_labs_playbook.R
+++ mobile_labs_playbook.R
@@ -3,6 +3,7 @@ library(googleAnalyticsR)
 library(lubridate)
 library(googlesheets)
 library(reshape2)
+library(zoo)
 
 
 ga_auth(new_user = TRUE)
@@ -22,7 +23,7 @@ seg_allUsers <- segment_ga4("All Users", segment_id = segment_for_allusers)
 
 # enter start date and end date here. Format: yyyy-mm-dd
 startDate <- "2017-11-13"
-endDate <- "2017-12-03"
+endDate <- "2018-01-15"
 
 # Slide 11 - Current State of Play
 # ecommerce conversion rate is used here but other conversion types can be included too
@@ -30,11 +31,15 @@ endDate <- "2017-12-03"
 ga_data_currentstate <- 
   google_analytics_4(view_id, #=This is a (dynamic) ViewID parameter
                      date_range = c(startDate, endDate), 
-                     metrics = c("sessions", "transactions"), 
+                     metrics = c("sessions", "transactions", "transactionRevenue"), 
                      dimensions = c("yearMonth", "deviceCategory", "userType"),
                      segments = c(seg_allUsers),
                      anti_sample = TRUE,
                      max = -1)
+
+ga_data_currentstate <-  ga_data_currentstate %>%
+  mutate(yearMonth = as.yearmon(as.character(yearMonth), "%Y%m"))
+
 
 chartData <- ga_data_currentstate %>%
   group_by(yearMonth) %>%
@@ -43,8 +48,64 @@ chartData <- ga_data_currentstate %>%
   mutate(transactionsPerSession = transactions / Sessions) %>%
   select(-transactions) %>%
   rename(`Conversion Rate` = transactionsPerSession)
-  
 
+# traffic growth rate by device
+currentState_trafficGrowth_device <- ga_data_currentstate %>%
+  select(yearMonth, deviceCategory, sessions) %>%
+  group_by(deviceCategory, yearMonth) %>%
+  summarize(sessions = sum(sessions))
+
+currentState_trafficGrowth_device <- currentState_trafficGrowth_device %>%
+  filter((yearMonth == min(yearMonth) | yearMonth == max(yearMonth))) %>%
+  spread(yearMonth, sessions)
+
+currentState_trafficGrowth_device$`% change` <- (currentState_trafficGrowth_device[[3]]-currentState_trafficGrowth_device[[2]])/currentState_trafficGrowth_device[[2]]
+currentState_trafficGrowth_device$`% change` <- round(currentState_trafficGrowth_device$`% change`, 3)  
+  
+# traffic growth rate by new vs returning
+currentState_trafficGrowth_userType <- ga_data_currentstate %>%
+  select(yearMonth, userType, sessions) %>%
+  group_by(userType, yearMonth) %>%
+  summarize(sessions = sum(sessions))
+
+currentState_trafficGrowth_userType <- currentState_trafficGrowth_userType %>%
+  filter((yearMonth == min(yearMonth) | yearMonth == max(yearMonth))) %>%
+  spread(yearMonth, sessions)
+
+currentState_trafficGrowth_userType$`% change` <- (currentState_trafficGrowth_userType[[3]]-currentState_trafficGrowth_userType[[2]])/currentState_trafficGrowth_userType[[2]]
+currentState_trafficGrowth_userType$`% change` <- round(currentState_trafficGrowth_userType$`% change`, 3)  
+
+# traffic growth rate by new/returning and device category
+ga_data_currentstate_all <- ga_data_currentstate %>%
+  group_by(userType, deviceCategory, yearMonth) %>%
+  summarize(sessions= sum(sessions))
+
+currentState_trafficGrowth_all <- ga_data_currentstate_all %>%
+  filter((yearMonth == min(yearMonth) | yearMonth == max(yearMonth))) %>%
+  spread(yearMonth, sessions)
+
+currentState_trafficGrowth_all$`% change` <- (currentState_trafficGrowth_all[[4]]-currentState_trafficGrowth_all[[3]])/currentState_trafficGrowth_all[[3]]
+currentState_trafficGrowth_all$`% change` <- round(currentState_trafficGrowth_all$`% change`, 3)  
+
+# Transactions & Revenue growth rate
+ga_data_currentstate_txnrevenue <- ga_data_currentstate %>%
+  group_by(deviceCategory, yearMonth) %>%
+  summarize(transactions = sum(transactions),
+            transactionRevenue = sum(transactionRevenue)) %>%
+  filter((yearMonth == min(yearMonth) | yearMonth == max(yearMonth))) %>%
+  gather(variable, value, -(yearMonth:deviceCategory)) %>%
+  unite(temp, yearMonth, variable) %>%
+  spread(temp, value)
+
+# Table for Ecommerce Conversion Rate by Device
+ga_data_currentstate_ecomm_conv <- ga_data_currentstate %>%
+  group_by(deviceCategory, yearMonth) %>%
+  summarize(transactions = sum(transactions),
+            sessions = sum(sessions)) %>%
+  filter((yearMonth == min(yearMonth) | yearMonth == max(yearMonth))) %>%
+  gather(variable, value, -(yearMonth:deviceCategory)) %>%
+  unite(temp, yearMonth, variable) %>%
+  spread(temp, value)
 
 
 # upload data to Googlesheets - what if owner of google sheet is different
@@ -53,4 +114,10 @@ myworksheet <- gs_key("1ENbfT76-Q_HcmvzuePMTqQjF3EU4IJxMevJ4rgdNK3c")
 
 # alternative - get access to specified google sheets by title
 # myworksheet <- gs_title("Sendo_Measurement Deck")
-gs_edit_cells(myworksheet, ws = "GA Data", input = chartData, anchor = "A3")
+gs_edit_cells(myworksheet, ws = "GA Data", input = chartData, anchor = "J3")
+gs_edit_cells(myworksheet, ws = "GA Data", input = currentState_trafficGrowth_device, anchor = "A17")
+gs_edit_cells(myworksheet, ws = "GA Data", input = currentState_trafficGrowth_userType, anchor = "A24") 
+gs_edit_cells(myworksheet, ws = "GA Data", input = currentState_trafficGrowth_all, anchor = "A30") 
+gs_edit_cells(myworksheet, ws = "GA Data", input = ga_data_currentstate_txnrevenue, anchor = "A39")
+gs_edit_cells(myworksheet, ws = "GA Data", input = ga_data_currentstate_ecomm_conv, anchor = "A45") 
+
