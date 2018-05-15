1bedc09c36bf1089aa6a532bd9ef5998ac702eb7
khzt2004 <khzt@hotmail.com>
2018-05-15 17:07:34 +0800

    Add files via upload

--- guid_lollipopchart.R
+++ guid_lollipopchart.R
@@ -14,7 +14,7 @@ data1 <- data %>%
   select(Market = A_market,
          'Tier Points Level' = 'A_tier_points_level', 3:6) %>%
   mutate(bookingLeadMonths = round(bookingLeadMonths,2)) %>%
-  filter(B_AncillaryCategory == 'hotel') %>%
+  filter(B_AncillaryCategory == 'insurance') %>%
   group_by(Market) %>%
   summarise(`Booking Lead Months` = round(mean(bookingLeadMonths),2))
 
@@ -34,7 +34,7 @@ data2 <- data %>%
   select(Market = A_market,
          'Tier Points Level' = 'A_tier_points_level', 3:6) %>%
   mutate(bookingLeadMonths = round(bookingLeadMonths,2)) %>%
-  filter(B_AncillaryCategory == 'hotel') %>%
+  filter(B_AncillaryCategory == 'insurance') %>%
   group_by(`Tier Points Level`) %>%
   summarise(`Booking Lead Months` = round(mean(bookingLeadMonths),2)) %>%
   filter(!is.na(`Tier Points Level`))
@@ -50,3 +50,15 @@ ggplot(data2, aes(y=`Tier Points Level`, x=`Booking Lead Months`)) +
   theme(axis.text.y=element_text(margin=margin(r=0, l=0))) +
   theme(plot.margin=unit(rep(30, 4), "pt")) +
   theme(plot.title=element_text(face="bold"))
+
+
+
+# histogram for exploration of distributions
+
+data_hist <- data %>%
+#  filter(grepl("insurance|car|hotel|taxi",B_AncillaryCategory) & !is.na(A_market))
+  filter(grepl("insurance",B_AncillaryCategory) & !is.na(A_market))
+  
+ggplot(data_hist, aes(x=bookingLeadMonths)) +
+  geom_histogram(position="identity", colour="grey40", alpha=0.2, bins = 10) +
+  facet_grid(. ~ A_market)
