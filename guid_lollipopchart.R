a2b6be4d5d4098847cd94dcdcf2faaae77ec44af
khzt2004 <khzt@hotmail.com>
2018-05-18 18:18:46 +0800

    Add files via upload

--- guid_lollipopchart.R
+++ guid_lollipopchart.R
@@ -4,29 +4,28 @@ library(scales)
 library(ggthemes)
 
 
-data <- read_csv("ancillary_chart.csv")
+data <- read_csv("ancillary_chart2.csv")
 data <- data %>%
-  mutate(B_AncillaryCategory = case_when(grepl("taxi|taxivan|shuttle", B_AncillaryCategory, ignore.case = TRUE) ~"taxi",
-                                         TRUE ~ (as.character(B_AncillaryCategory))),
-         A_tier_points_level = case_when(A_tier_points_level == 'ae' ~ "elite",
-                                         A_tier_points_level == 'ag' ~ "gold",
-                                         A_tier_points_level == 'as' ~ "silver",
-                                         A_tier_points_level == 'aj' ~ "base"))
+  mutate(ancillaryCategory = case_when(grepl("taxi|taxivan|shuttle", productName, ignore.case = TRUE) ~"taxi",
+                                       productName == 'car' ~ "car",
+                                       productName == 'insurance' ~ "insurance",
+                                         TRUE ~ (as.character(ancillaryCategory))),
+         tier_points_level = case_when(tier_points_level == 'ae' ~ "elite",
+                                         tier_points_level == 'ag' ~ "gold",
+                                         tier_points_level == 'as' ~ "silver",
+                                         tier_points_level == 'aj' ~ "base"))
 # data_raw <- read_csv("https://raw.githubusercontent.com/khzt2004/R/master/ancillary_chart.csv")
 
 data1 <- data %>%
-  select(Market = A_market,
-         'Tier Points Level' = 'A_tier_points_level', 3:6) %>%
-  mutate(bookingLeadMonths = round(bookingLeadMonths,2)) %>%
-  filter(B_AncillaryCategory == 'taxi') %>%
-  group_by(Market) %>%
-  summarise(`Booking Lead Months` = round(mean(bookingLeadMonths),2))
+  filter(ancillaryCategory == 'taxi') %>%
+  group_by(market) %>%
+  summarise(`Booking Lead Days` = round(mean(booking_lead_days),0))
 
 # breakdown by market
-ggplot(data1, aes(y=Market, x=`Booking Lead Months`)) + 
+ggplot(data1, aes(y=market, x=`Booking Lead Days`)) + 
   geom_lollipop(point.colour="steelblue", point.size=2, horizontal=TRUE) +
   theme_minimal() +
-  geom_label(aes(label=`Booking Lead Months`), nudge_x = 0.25, size=3) +
+  geom_label(aes(label=`Booking Lead Days`), nudge_x = 0.4, size=3) +
   theme(panel.grid.major.y=element_blank()) +
   theme(panel.grid.minor=element_blank()) +
   theme(axis.line.y=element_line(color="#2b2b2b", size=0.15)) +
@@ -35,19 +34,19 @@ ggplot(data1, aes(y=Market, x=`Booking Lead Months`)) +
   theme(plot.title=element_text(face="bold"))
 
 data2 <- data %>%
-  select(Market = A_market,
-         'Tier Points Level' = 'A_tier_points_level', 3:6) %>%
-  mutate(bookingLeadMonths = round(bookingLeadMonths,2)) %>%
-  filter(B_AncillaryCategory == 'taxi') %>%
+  select(ancillaryCategory, market,
+         'Tier Points Level' = 'tier_points_level', 3:6) %>%
+  mutate(bookingLeaddays = round(booking_lead_days,0)) %>%
+  filter(ancillaryCategory == 'taxi') %>%
   group_by(`Tier Points Level`) %>%
-  summarise(`Booking Lead Months` = round(mean(bookingLeadMonths),2)) %>%
+  summarise(`Booking Lead Days` = round(mean(bookingLeaddays),0)) %>%
   filter(!is.na(`Tier Points Level`))
 
 # breakdown by tier
-ggplot(data2, aes(y=`Tier Points Level`, x=`Booking Lead Months`)) + 
+ggplot(data2, aes(y=`Tier Points Level`, x=`Booking Lead Days`)) + 
   geom_lollipop(point.colour="steelblue", point.size=2, horizontal=TRUE) +
   theme_minimal() +
-  geom_label(aes(label=`Booking Lead Months`), nudge_x = 0.25, size=3) +
+  geom_label(aes(label=`Booking Lead Days`), nudge_x = 0.25, size=3) +
   theme(panel.grid.major.y=element_blank()) +
   theme(panel.grid.minor=element_blank()) +
   theme(axis.line.y=element_line(color="#2b2b2b", size=0.15)) +
@@ -61,9 +60,9 @@ ggplot(data2, aes(y=`Tier Points Level`, x=`Booking Lead Months`)) +
 
 data_hist <- data %>%
 #  filter(grepl("insurance|car|hotel|taxi",B_AncillaryCategory) & !is.na(A_market))
-  filter(grepl("car",B_AncillaryCategory) & !is.na(A_market)  )
+  filter(grepl("insurance", ancillaryCategory) & !is.na(market)  )
   
-ggplot(data_hist, aes(x=bookingLeadMonths, fill=A_tier_points_level)) +
+ggplot(data_hist, aes(x=booking_lead_days, fill=tier_points_level)) +
   geom_histogram(position="identity", colour="grey40", alpha=0.2, bins = 10) +
-  facet_grid(.~ A_tier_points_level) + 
-  labs(fill = "Tier Points Level\n")
+  facet_grid(.~ tier_points_level) + 
+  labs(title = "Ancillary Product - Car", fill = "Tier Points Level\n")
