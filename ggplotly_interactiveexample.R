8f4ffa6ccdacf3bedb9bb95857b8bce6f35339fa
khzt2004 <khzt@hotmail.com>
2019-03-07 13:44:36 +0800

    Add files via upload

--- ggplotly_interactiveexample.R
+++ ggplotly_interactiveexample.R
@@ -3,7 +3,13 @@ library(plotly)
 
 # https://datatitian.com/how-to-turn-your-ggplot2-visualization-into-an-interactive-tweet/
 
+# df <- read_csv("clusterable_embedding_df.csv")
+
 static_plot <- ggplot(iris, aes(x = Petal.Length, y = Petal.Width, color = Species)) +
   geom_point()
+
+# static_plot <- ggplot(df, aes(x = `0`, y = `1`, color = Desc_00001)) +
+#   geom_point()
+
 interactive_plot <- plotly::ggplotly(static_plot)
 interactive_plot
